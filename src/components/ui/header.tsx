c43a72339423f66f1a848150580860724ac8c87f
Mateus Souza <msouza954@gmail.com>
2023-10-17 21:03:45 -0300

    feat: add header menu using sheet components

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,20 +1,62 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react"
-import { Button } from "./button"
-import { Card } from "./card"
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentCircleIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
+import { Button } from "./button";
+import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 export const Header = () => {
-  return ( 
+  return (
     <Card className="flex justify-between p-[1.875rem]">
-      <Button size="icon" variant="outline">
-        <MenuIcon/>
-      </Button> 
+      <Sheet>
+        <SheetTrigger>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-3">
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <HomeIcon />
+              Início
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <LogInIcon />
+              Fazer Login
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <PercentIcon />
+              Ofertas
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <ListOrderedIcon />
+              Catálago
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
 
       <h1 className="text-lg font-semibold">
         <span className="text-primary">FSW</span> Luastore
       </h1>
 
       <Button size="icon" variant="outline">
-        <ShoppingCartIcon/>
-      </Button> 
-    </Card> 
-)}
+        <ShoppingCartIcon />
+      </Button>
+    </Card>
+  );
+};
