4bedfe44b002097bf32a4e3830f18a8717435230
Mateus Souza <msouza954@gmail.com>
2023-10-23 16:58:35 -0300

    feat: Categories anda CategoriesItemsProps

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,23 @@
 "use client";
 
 import Image from "next/image";
+import { Categories } from "./components/categories";
 
 export default function Home() {
   return (
-    <>
+    <div className="p-5">
       <Image
-        src="/baner-home-01.svg"
-        alt="Baner Home 01"
+        src="/banner-home-01.svg"
+        alt="Banner Home 01"
         width={0}
         height={0}
         className="h-auto w-full"
         sizes="100vh"
       />
-    </>
+
+      <div className="mt-8">
+        <Categories />
+      </div>
+    </div>
   );
 }
